dd746979544b4e6504c484542cfac5da2404f17e
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 23:24:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ class Driver {
   
  
   yearsExperienceFromBeginningOf (year) {
-    let endDate = new Date( year, 1, 1 )
-    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 )
-      return parseInt( totalYears )
-    
+   yearsExperienceFromBeginningOf (endDate) {
+    return year - this.startDate.getFullYear()
   }
 }
 
